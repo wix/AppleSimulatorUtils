d9b692667300d585fa417001626ac19b23a22200
Adam Wilson <adam@codegarden.co.uk>
2017-08-24 15:20:46 +0100

    Change `build` to `applesimutils` to fix build error

--- buildForBrew.sh
+++ buildForBrew.sh
@@ -2,4 +2,4 @@ targetDir="$1"
 
 xcodebuild clean build -project applesimutils/applesimutils.xcodeproj -scheme applesimutils -configuration Release -derivedDataPath ./build
 mkdir -p "$targetDir"/bin
-cp build/Build/Products/Release/applesimutils "$targetDir"/bin
+cp applesimutils/Build/Products/Release/applesimutils "$targetDir"/bin
