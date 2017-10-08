4ef1185c46945f6993225418e98a48d3dc2fa53f
Leo Natan <leo.natan@outlook.com>
2017-10-08 17:03:44 +0300

    Add BUILD_DIR override in attempt to fix build issues for some people

--- buildForBrew.sh
+++ buildForBrew.sh
@@ -1,5 +1,5 @@
 targetDir="$1"
 
-xcodebuild clean build -project applesimutils/applesimutils.xcodeproj -scheme applesimutils -configuration Release -derivedDataPath ./build
+xcodebuild clean build -project applesimutils/applesimutils.xcodeproj -scheme applesimutils -configuration Release -derivedDataPath ./build BUILD_DIR=../build/Build/Products
 mkdir -p "$targetDir"/bin
 cp build/Build/Products/Release/applesimutils "$targetDir"/bin
