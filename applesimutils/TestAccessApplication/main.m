727d98509ebb1296a83cab7fdc4aed3212f70f0d
Andrew Romanov <scalli-k-be@ya.ru>
2018-10-01 18:11:09 +0700

    added test application

---
+++ applesimutils/TestAccessApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestAccessApplication
+//
+//  Created by Andrew Romanov on 01/10/2018.
+//  Copyright © 2018 Wix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
