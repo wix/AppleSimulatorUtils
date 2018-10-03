453fd8cc6d2c951dbe707bf6e1bb58fc4918f0a2
Leo Natan <leo.natan@outlook.com>
2018-10-03 21:01:31 +0300

    Add install notice for upgrading CLT

--- buildForBrew.sh
+++ buildForBrew.sh
@@ -1,5 +1,7 @@
 targetDir="$1"
 
+echo "\n\n\033[1;33mIf you see issues while installing applesimutils, make sure to update your Xcode Commandline Tools from System Preferences\033[0m\n\n"
+
 export CODE_SIGNING_REQUIRED=NO && xcodebuild clean build -project applesimutils/applesimutils.xcodeproj -scheme applesimutils -configuration Release -derivedDataPath ./build BUILD_DIR=../build/Build/Products
 mkdir -p "$targetDir"/bin
 cp build/Build/Products/Release/applesimutils "$targetDir"/bin
