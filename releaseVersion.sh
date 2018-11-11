8da31517365665185744800dc06220a42e433476
Leo Natan <leo.natan@outlook.com>
2018-11-11 14:02:09 +0200

    Create github release in release script

--- releaseVersion.sh
+++ releaseVersion.sh
@@ -26,26 +26,38 @@ fi
 
 echo "Creating commit for version"
 
-echo "\"$1\"" > applesimutils/applesimutils/version.h
+VERSION="$1"
+
+echo "\"$VERSION\"" > applesimutils/applesimutils/version.h
 
 echo "Cleaning up"
 
 git clean -xdf
 
-echo "Updating CHANGELOG"
+echo "Creating release notes"
 
-github_changelog_generator --future-release "$1" --bugs-label "**Fixed Bugs**" --enhancement-label "**Enhancements**" --issues-label "**Closed Issues**" --pr-label "**Merged Pull Requests**" --no-verbose
+RELEASE_NOTES_FILE=._tmp_release_notes.md
+
+touch "${RELEASE_NOTES_FILE}"
+open -Wn "${RELEASE_NOTES_FILE}"
+
+if ! [ -s "${RELEASE_NOTES_FILE}" ]; then
+	echo -e >&2 "\033[1;31mNo release notes provided, aborting.\033[0m"
+	rm -f "${RELEASE_NOTES_FILE}"
+	exit -1
+fi
 
 echo "Creating a compressed tarball of the source"
 
-tar --exclude=".git" --exclude=".github" --exclude="homebrew-brew" -cvzf "AppleSimulatorUtils-$1.tar.gz" .
+tar --exclude=".git" --exclude "._tmp_release_notes.md" --exclude=".github" --exclude="homebrew-brew" -cvzf "AppleSimulatorUtils-$1.tar.gz" .
 
 echo "Updating brew repository with latest tarball and update applesimutils.rb"
 
 cd homebrew-brew
 
-git fetch
 git checkout master
+git fetch
+git pull --rebase
 mv "../AppleSimulatorUtils-$1.tar.gz" .
 sed -i '' -e 's/url .*/url '"'https:\/\/raw.githubusercontent.com\/wix\/homebrew-brew\/master\/AppleSimulatorUtils-$1.tar.gz'"'/g' applesimutils.rb
 sed -i '' -e 's/sha256 .*/sha256 '"'"$(shasum -b -a 256 AppleSimulatorUtils-$1.tar.gz | awk '{ print $1 }')"'"'/g' applesimutils.rb
@@ -64,4 +76,11 @@ git tag "$1"
 git push
 git push --tags
 
-# version.h
+echo "Creating release"
+
+#Escape user input in markdown to valid JSON string using PHP 🤦‍♂️ (https://stackoverflow.com/a/13466143/983912)
+RELEASENOTESCONTENTS=$(printf '%s' "$(<"${RELEASE_NOTES_FILE}")" | php -r 'echo json_encode(file_get_contents("php://stdin"));')
+API_JSON=$(printf '{"tag_name": "%s","target_commitish": "master", "name": "%s", "body": %s, "draft": false, "prerelease": false}' "$VERSION" "$VERSION" "$RELEASENOTESCONTENTS")
+RELEASE_ID=$(curl -s --data "$API_JSON" https://api.github.com/repos/wix/AppleSimulatorUtils/releases?access_token=${GITHUB_RELEASES_TOKEN} | jq ".id")
+
+rm -f "${RELEASE_NOTES_FILE}"
