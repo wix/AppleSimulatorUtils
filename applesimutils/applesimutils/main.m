49a35698730fb039a22afe735f7975bf7fdbf04d
Zach Smith <zach@commure.com>
2019-02-07 09:08:06 -0800

    Fix whitespace

--- applesimutils/applesimutils/main.m
+++ applesimutils/applesimutils/main.m
@@ -98,11 +98,11 @@ static NSArray* simulatorDevicesList()
 	
 	[runtimes enumerateObjectsUsingBlock:^(NSDictionary<NSString*, id>* _Nonnull runtime, NSUInteger idx, BOOL * _Nonnull stop) {
 		NSString* runtimeName = runtime[@"name"];
-        NSString* runtimeIdentifier = runtime[@"identifier"];
-        NSArray* nameDevices = devices[runtimeName] ?: @[];
-        NSArray* identifierDevices = devices[runtimeIdentifier] ?: @[];
+		NSString* runtimeIdentifier = runtime[@"identifier"];
+		NSArray* nameDevices = devices[runtimeName] ?: @[];
+		NSArray* identifierDevices = devices[runtimeIdentifier] ?: @[];
 		NSArray* runtimeDevices = [nameDevices arrayByAddingObjectsFromArray:identifierDevices];
-        NSArray* filteredDevices = [runtimeDevices filteredArrayUsingPredicate:availabilityPredicate];
+		NSArray* filteredDevices = [runtimeDevices filteredArrayUsingPredicate:availabilityPredicate];
 		[filteredDevices setValue:runtime forKey:@"os"];
 		[allDevices addObjectsFromArray:runtimeDevices];
 	}];
