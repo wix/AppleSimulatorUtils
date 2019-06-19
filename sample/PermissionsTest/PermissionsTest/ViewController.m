4bfde0607b56c7599c5332ccfed70dd16193d0b6
Leo Natan <leo.natan@outlook.com>
2019-06-19 17:59:32 +0300

    Add HealthKit to testing project.

--- sample/PermissionsTest/PermissionsTest/ViewController.m
+++ sample/PermissionsTest/PermissionsTest/ViewController.m
@@ -10,11 +10,13 @@
 @import EventKit;
 @import CoreLocation;
 @import UserNotifications;
+@import HealthKit;
 
 @interface ViewController () <CLLocationManagerDelegate>
 {
 	EKEventStore* _eventStore;
 	CLLocationManager* _locationManager;
+	HKHealthStore* _healthStore;
 }
 
 @end
@@ -28,6 +30,8 @@
 	_eventStore = [EKEventStore new];
 	_locationManager = [CLLocationManager new];
 	_locationManager.delegate = self;
+	
+	_healthStore = [HKHealthStore new];
 	
 	NSLog(@"%@", NSBundle.mainBundle.bundleURL.absoluteString);
 }
@@ -50,7 +54,16 @@
 	[center requestAuthorizationWithOptions: UNAuthorizationOptionAlert | UNAuthorizationOptionSound completionHandler:^(BOOL granted, NSError * _Nullable error) {
 		NSLog(@"Notifications: %@", granted ? @"<granted>" : @"<not granted>");
 	}];
+}
+
+- (IBAction)_health:(id)sender
+{
+//	NSSet* writableTypes = [NSSet setWithObject:[HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierPeakExpiratoryFlowRate]];
+	NSSet* readableTypes = [NSSet setWithObjects:[HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierDistanceWalkingRunning], [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierBloodPressureSystolic], nil];
 	
+	[_healthStore requestAuthorizationToShareTypes:nil readTypes:readableTypes completion:^(BOOL success, NSError * _Nullable error) {
+		NSLog(@"Health: %@", success ? @"<granted>" : [NSString stringWithFormat:@"<not granted: %@>", error]);
+	}];
 }
 
 - (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
