39e19cbc2ac5e7f0de2be25a20b1bdb857a07112
Simon Reynolds <reynolds.simon4@gmail.com>
2023-01-27 16:20:15 +0100

    Added critical flag to notifications

--- applesimutils/applesimutils/SetNotificationsPermission.m
+++ applesimutils/applesimutils/SetNotificationsPermission.m
@@ -20,7 +20,12 @@
 		return [self _setSectionInfoData:nil forBundleIdentifier:bundleIdentifier displayName:displayName simulatorIdentifier:simulatorId error:error];
 	}
 	
-	BOOL enabled = [status boolValue];
+    BOOL enabled;
+	if([status isEqualToString:@"critical"]) {
+        enabled = TRUE;
+	} else {
+		enabled = [status boolValue];
+	} 
 	
 	static NSString* const b64S = @"YnBsaXN0MDDUAQIDBAUGTU5YJHZlcnNpb25YJG9iamVjdHNZJGFyY2hpdmVyVCR0b3ASAAGGoKgHCDAxQUgfSVUkbnVsbN8QFQkKCwwNDg8QERITFBUWFxgZGhscHR4fHiEiIx4jJicfHygjIh8jIyMjI18QFHN1cHByZXNzRnJvbVNldHRpbmdzXxASc3VwcHJlc3NlZFNldHRpbmdzWmhpZGVXZWVBcHBZc2VjdGlvbklEW2Rpc3BsYXlOYW1lVGljb25fEBlkaXNwbGF5c0NyaXRpY2FsQnVsbGV0aW5zW3N1YnNlY3Rpb25zXxATc2VjdGlvbkluZm9TZXR0aW5nc1YkY2xhc3NfEA9zZWN0aW9uQ2F0ZWdvcnlfEBJzdWJzZWN0aW9uUHJpb3JpdHlXdmVyc2lvbl8QGm1hbmFnZWRTZWN0aW9uSW5mb1NldHRpbmdzV2FwcE5hbWVbc2VjdGlvblR5cGVfEBBmYWN0b3J5U2VjdGlvbklEXxAPZGF0YVByb3ZpZGVySURzXHN1YnNlY3Rpb25JRFdmaWx0ZXJzXxAYcGF0aFRvV2VlQXBwUGx1Z2luQnVuZGxlCBAACIACgAWAAAiAAIADgAeABoAAgAWAAIAAgACAAIAAXxAmY29tLkxlb05hdGFuLkxOUG9wdXBDb250cm9sbGVyRXhhbXBsZS3ZMjM0NTY3Ejg5Ojs7Ox8fPjtAXHB1c2hTZXR0aW5nc18QGXNob3dzSW5Ob3RpZmljYXRpb25DZW50ZXJfEBNhbGxvd3NOb3RpZmljYXRpb25zXxAWc2hvd3NPbkV4dGVybmFsRGV2aWNlc18QFWNvbnRlbnRQcmV2aWV3U2V0dGluZ15jYXJQbGF5U2V0dGluZ18QEXNob3dzSW5Mb2NrU2NyZWVuWWFsZXJ0VHlwZRA/CQkJgAQJEAHSQkNERVokY2xhc3NuYW1lWCRjbGFzc2VzXxAVQkJTZWN0aW9uSW5mb1NldHRpbmdzokZHXxAVQkJTZWN0aW9uSW5mb1NldHRpbmdzWE5TT2JqZWN0V0xOUG9wdXDSQkNKS11CQlNlY3Rpb25JbmZvokxHXUJCU2VjdGlvbkluZm9fEA9OU0tleWVkQXJjaGl2ZXLRT1BUcm9vdIABAAgAEQAaACMALQAyADcAQABGAHMAigCfAKoAtADAAMUA4QDtAQMBCgEcATEBOQFWAV4BagF9AY8BnAGkAb8BwAHCAcMBxQHHAckBygHMAc4B0AHSAdQB1gHYAdoB3AHeAeACCQIcAikCRQJbAnQCjAKbAq8CuQK7ArwCvQK+AsACwQLDAsgC0wLcAvQC9wMPAxgDIAMlAzMDNgNEA1YDWQNeAAAAAAAAAgEAAAAAAAAAUQAAAAAAAAAAAAAAAAAAA2A=";
 	
@@ -28,9 +33,16 @@
 	
 	NSDictionary* propList = CFBridgingRelease(CFPropertyListCreateWithData(NULL, (__bridge CFDataRef)b64, kCFPropertyListMutableContainersAndLeaves, NULL, NULL));
 	
-	propList[@"$objects"][2] = bundleIdentifier;
-	propList[@"$objects"][3][@"allowsNotifications"] = @(enabled);
-	propList[@"$objects"][5] = displayName;
+	if([status isEqualToString:@"critical"]) {
+		propList[@"$objects"][2] = bundleIdentifier;
+		propList[@"$objects"][3][@"allowsNotifications"] = @(enabled);
+		propList[@"$objects"][3][@"criticalAlertSetting"] = enabled ? @2 : @1;
+		propList[@"$objects"][5] = displayName;
+	} else {
+		propList[@"$objects"][2] = bundleIdentifier;
+		propList[@"$objects"][3][@"allowsNotifications"] = @(enabled);
+		propList[@"$objects"][5] = displayName;
+	}
 	
 	NSData* sectionInfoData = CFBridgingRelease(CFPropertyListCreateData(NULL, (__bridge CFTypeRef)propList, kCFPropertyListBinaryFormat_v1_0, 0, NULL));
 
